e5730ec7bbaeb89751c56ca25ee6f1d7b2d4ea9c
travis-konarik <travis_konarik@outlook.com>
2018-04-10 19:43:37 -0400

    Updated calendar to run on api @ https://event-parking-api.herokuapp.com/event

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
-const Logo = ({ logoSrc }) => {
-  return(
-    <div>
-      <img src={ logoSrc }></img>
-    </div>
-  );
-}
-
-export default Logo;
+// import React from 'react';
+//
+// const Logo = ({ logoSrc }) => {
+//   return(
+//     <div>
+//       <img src={ logoSrc }></img>
+//     </div>
+//   );
+// }
+//
+// export default Logo;
--- src/containers/CalendarContainer/CalendarContainer.js
+++ src/containers/CalendarContainer/CalendarContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import Spinner from 'react-spinkit';
@@ -6,79 +6,83 @@ import BigCalendar from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import _ from 'underscore';
 
+
 export default class CalendarContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      dates: [],
-      events: [],
-      isParking: false,
-      isLoaded: false,
+    constructor() {
+        super();
+        this.state = {
+            dates: [],
+            events: [],
+            isParking: false,
+            isLoaded: false,
+        };
+        BigCalendar.momentLocalizer(moment);
     }
-    BigCalendar.momentLocalizer(moment);
-  }
 
-  componentDidMount() {
-    const eventsURL = "https://cfo-event-parking.herokuapp.com/events";
-    axios.get(eventsURL, {
-      crossDomain: true,
-      headers: {'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'HEAD, GET, POST, PUT, PATCH, DELETE',
-                'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'}
-    }).then((res) => { this.setState({ dates: res.data }); })
-      .then((res) => {
-        let updatedEvents = [];
-        _.each(this.state.dates, (date) => {
-          let pDate = date.split('-');
-          updatedEvents.push({
-             'title': 'Event Parking',
-             'start': new Date(Number(pDate[0]), Number(pDate[1])-1, Number(pDate[2])),
-             'end': new Date(Number(pDate[0]), Number(pDate[1])-1, Number(pDate[2])),
-          });
-        });
-        this.setState({ events: updatedEvents });
-      }).then((res) => {
-        if(_.some(this.state.dates, (date) => {
-          return date === moment().format('YYYY-MM-DD');
-        })) {
-          this.setState({ isParking: true });
+    componentDidMount() {
+        const eventsURL = "https://event-parking-api.herokuapp.com/event/2018/4";
+        axios.get(eventsURL).then((res) => {
+            this.setState({events: CalendarContainer.formatRows(res.data.rows)});
+            this.setParkingState(res.data.rows);
+            this.setState({isLoaded: true});
+        })
+    }
+
+    render() {
+        let parkingMessage = null, calendar = null;
+        if (this.state.isLoaded) {
+            calendar = <BigCalendar
+                events={this.state.events}
+                views={{
+                    'month': true,
+                    'agenda': false,
+                    'week': false,
+                    'day': false,
+                    'work_week': false
+                }}
+                toolbar={false}
+            />
+            if (this.state.isParking) {
+                parkingMessage =
+                    <h2 style={{textAlign: "center", margin: "20px 0 20px 0"}}>YES! There is Event Parking Today!</h2>
+            }
+            else {
+                parkingMessage =
+                    <h2 style={{textAlign: "center", margin: "20px 0 20px 0"}}>There is no Event Parking today.</h2>
+            }
+        } else {
+            parkingMessage = <h2 style={{textAlign: "center", margin: "20px 0 20px 0"}}>Loading...</h2>
+            calendar = <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "300px"}}>
+                <Spinner name="ball-spin-fade-loader"/>
+            </div>
         }
-      }).then((res) => {
-          this.setState({ isLoaded: true }); 
-      })
+        return (
+            <div>
+                {parkingMessage}
+                {calendar}
+            </div>
+        );
     }
 
-  render() {
-    let parkingMessage = null, calendar = null;
-    if (this.state.isLoaded) {
-        calendar = <BigCalendar 
-        events={this.state.events} 
-        views={{
-          'month': true,
-          'agenda': false,
-          'week': false,
-          'day': false,
-          'work_week': false
-        }}
-        toolbar={false}
-      />
-      if (this.state.isParking) {
-        parkingMessage = <h2 style={{textAlign: "center", margin:"20px 0 20px 0"}}>YES! There is Event Parking Today!</h2>
-      }
-      else {
-        parkingMessage = <h2 style={{textAlign: "center", margin:"20px 0 20px 0"}}>There is no Event Parking today.</h2>
-      }
-    } else { 
-        parkingMessage = <h2 style={{textAlign: "center", margin: "20px 0 20px 0"}}>Loading...</h2>
-        calendar = <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "300px" }}> 
-                       <Spinner name="ball-spin-fade-loader" />
-                   </div>
+    static formatRows(rows) {
+        _.each(rows, (row) => {
+            // console.log(row.start_date);
+            // console.log(row.end_date);
+            row['start'] = row.start_date;
+            row['end'] = row.end_date;
+
+        });
+        return rows;
     }
-    return(
-      <div>
-        { parkingMessage }
-        { calendar }
-      </div>
-    );
-  }
+
+    setParkingState(events) {
+        this.setState({
+            isParking: _.some(events, (event) => {
+                return moment().isSame(event.start, 'day') ||
+                    moment().isSame(event.end, 'day');
+            })
+        });
+
+    }
 }
+
